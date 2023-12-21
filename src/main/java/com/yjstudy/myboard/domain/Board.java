7232304053d0b09a5c2806c9b8a94c9cb6193e66
H-Yeji <yeji980603@naver.com>
2023-12-22 00:23:54 +0900

    엔티티 개발

---
+++ src/main/java/com/yjstudy/myboard/domain/Board.java
@@ -0,0 +1,23 @@
+package com.yjstudy.myboard.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Board {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String writer;
+
+    private String title;
+
+    private String content;
+}
---
+++ src/main/java/com/yjstudy/myboard/domain/Member.java
@@ -0,0 +1,32 @@
+package com.yjstudy.myboard.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotBlank
+    private String loginId;
+
+    @NotBlank
+    private String password;
+
+    @Email
+    private String email;
+
+}
