0ce6eb4e1f8b8080297aa6d271d693b633932c0a
H-Yeji <yeji980603@naver.com>
2023-12-23 23:02:39 +0900

    게시글 수정 컨트롤러 및 뷰 개발

--- src/main/java/com/yjstudy/myboard/web/controller/BoardController.java
+++ src/main/java/com/yjstudy/myboard/web/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.yjstudy.myboard.repository.BoardRepository;
 import com.yjstudy.myboard.service.BoardService;
 import com.yjstudy.myboard.web.form.BoardForm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +14,9 @@ import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
-    private final BoardRepository boardRepository;
     private final BoardService boardService;
 
     /**
@@ -34,13 +35,13 @@ public class BoardController {
     public String registerPost(Board board) {
 
         boardService.addPost(board);
-        return "redirect:/boards/list"; //게시글 목록 페이지로 리다이렉트
+        return "redirect:/boards"; //게시글 목록 페이지로 리다이렉트
     }
 
     /**
      * 게시글 목록
      */
-    @GetMapping("/boards/list")
+    @GetMapping("/boards")
     public String boardList(Model model) {
 
         model.addAttribute("boardList", boardService.boardList());
@@ -50,11 +51,12 @@ public class BoardController {
     /**
      * 게시글 상세보기
      */
-    @GetMapping("/boards/list/{id}")
+    @GetMapping("/boards/{id}")
     public String boardView(@PathVariable int id, Model model) {
 
-        Optional<Board> board = boardRepository.findById(id);
-        model.addAttribute("board", board.orElse(null));
+        Board board = boardService.detail(id);
+
+        model.addAttribute("board", board);
 
         return "boards/boardView";
     }
@@ -62,11 +64,40 @@ public class BoardController {
     /**
      * 게시글 삭제
      */
-    @GetMapping("/boards/delete/{id}")
+    @GetMapping("/boards/{id}/delete")
     public String boardDelete(@PathVariable int id) {
 
         boardService.delete(id);
 
-        return "redirect:/boards/list";
+        return "redirect:/boards";
+    }
+
+    /**
+     * 상품 수정 폼
+     */
+    @GetMapping("/boards/{id}/edit")
+    public String boardEditForm(@PathVariable int id, Model model) {
+
+        Board board = boardService.detail(id); //id로 게시물 찾아옴
+
+        BoardForm form = new BoardForm();
+        form.setId(board.getId());
+        form.setWriter(board.getWriter());
+        form.setTitle(board.getTitle());
+        form.setContent(board.getContent());
+
+        model.addAttribute("boardForm", form);
+        return "boards/updateBoardForm";
+    }
+
+    /**
+     * 게시글 수정 진행
+     */
+    @PostMapping("/boards/{id}/edit")
+    public String boardEdit(@PathVariable int id, @ModelAttribute("boardForm") BoardForm boardForm) {
+
+        boardService.update(id, boardForm.getTitle(), boardForm.getContent());
+
+        return "redirect:/boards";
     }
 }
