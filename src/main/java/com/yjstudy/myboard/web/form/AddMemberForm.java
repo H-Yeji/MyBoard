0e0ceeca91814c773babf3f9c57f98254b06ca19
H-Yeji <yeji980603@naver.com>
2023-12-27 16:29:12 +0900

    회원가입 시 컬럼 조건 추가 및 에러메시지 코드 추가

--- src/main/java/com/yjstudy/myboard/web/form/AddMemberForm.java
+++ src/main/java/com/yjstudy/myboard/web/form/AddMemberForm.java
@@ -2,6 +2,7 @@ package com.yjstudy.myboard.web.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,11 @@ public class AddMemberForm {
     private String username;
 
     @NotBlank(message = "아이디는 필수입니다.")
+    @Pattern(regexp = "^[a-z0-9]{2,10}$", message = "아이디는 특수문자를 제외한 2~10자리여야 합니다.")
     private String loginId;
 
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
     @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 
