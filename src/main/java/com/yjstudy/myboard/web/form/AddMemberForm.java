8d30475c123b41ac40e43f1cfa9120a030242c37
H-Yeji <yeji980603@naver.com>
2024-01-29 17:05:49 +0900

    에러 메시지 수정

--- src/main/java/com/yjstudy/myboard/web/form/AddMemberForm.java
+++ src/main/java/com/yjstudy/myboard/web/form/AddMemberForm.java
@@ -10,17 +10,17 @@ import lombok.Setter;
 @Getter @Setter
 public class AddMemberForm {
 
-    @NotBlank(message = "이름은 필수입니다.")
+    @NotBlank(message = "{NotBlank.username}")
     private String username;
 
-    @NotNull(message = "아이디는 필수입니다.")
-    @Pattern(regexp = "^[a-z0-9]{2,10}$", message = "아이디는 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotNull(message = "{NotBlank.loginId}")
+    @Pattern(regexp = "^[a-z0-9]{2,10}$", message = "{Pattern.loginId}")
     private String loginId;
 
-    @NotNull(message = "비밀번호는 필수입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
+    @NotNull(message = "{NotBlank.password}")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "{Pattern.password}")
     private String password;
 
-    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="올바른 형식의 주소여야 합니다.")
+    @Pattern(regexp = "^$|^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "{Pattern.email}")
     private String email;
 }
