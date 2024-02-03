cb5f9f1a066f33d943771f3d6dfb87bedbed86e1
H-Yeji <yeji980603@naver.com>
2024-02-03 22:04:05 +0900

    home과 loginHome화면 구분해서 구현

--- src/main/java/com/yjstudy/myboard/web/controller/HomeController.java
+++ src/main/java/com/yjstudy/myboard/web/controller/HomeController.java
@@ -1,16 +1,41 @@
 package com.yjstudy.myboard.web.controller;
 
+import com.yjstudy.myboard.domain.Member;
+import com.yjstudy.myboard.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import static com.yjstudy.myboard.web.SessionConst.LOGIN_MEMBER;
+
 @Slf4j
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        log.info("home controller");
-        return "home";
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public String homeLogin(HttpServletRequest request, Model model) {
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(LOGIN_MEMBER);
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+        //세션이 유지되면 로그인 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
