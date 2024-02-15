57d1379b2263493c22f4694dfe3ab45e5e110d06
H-Yeji <yeji980603@naver.com>
2024-02-15 23:49:27 +0900

    파일 추가 관련 클래스 생성

---
+++ src/main/java/com/yjstudy/myboard/file/FileStore.java
@@ -0,0 +1,57 @@
+package com.yjstudy.myboard.file;
+
+import com.yjstudy.myboard.domain.UploadFile;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String uploadFileName = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(uploadFileName);
+
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(uploadFileName, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFileName) {
+
+        String ext = extractExt(originalFileName);
+        String uuid = UUID.randomUUID().toString();
+
+        return uuid + "." + ext; //uuid.ext를 저장소 파일명으로 설정
+
+    }
+
+    private String extractExt(String originalFileName) {
+
+        int pos = originalFileName.lastIndexOf("."); //.의 위치 찾기
+        return originalFileName.substring(pos+1); //.이후부터 끝까지 추출
+    }
+
+}
